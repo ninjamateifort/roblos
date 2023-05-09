6d7b6e27ff0d9cd9ac9ea85d52d570fb5a1b5ef4
ninjamateifort <65023740+ninjamateifort@users.noreply.github.com>
2023-05-09 04:30:15 +0000

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -2,6 +2,9 @@
 local Workspace = game:GetService("Workspace")
 local Player = game:GetService("Players").LocalPlayer
 local Mouse = Player:GetMouse()
+local SGames = {
+    6516141723
+}
 
 -- / Services
 local UserInputService = game:GetService("UserInputService")
@@ -29,6 +32,15 @@ local CreateTween = function(name, speed, style, direction, loop, reverse, delay
 
     TweenTable[name] = TweenInfo.new(speed, style, direction, loop, reverse, delay)
 end
+local function tablecontains(table, element)
+    for _, value in pairs(table) do
+      if value == element then
+        return true
+      end
+    end
+    return false
+  end
+  
 
 -- / Dragging
 local drag = function(obj, latency)
@@ -773,7 +785,11 @@ function library:Introduction()
     xsx.BackgroundTransparency = 1.000
     xsx.Size = UDim2.new(0, 80, 0, 21)
     xsx.Font = Enum.Font.Code
-    xsx.Text = ""
+    if (tablecontains(SGames,game.PlaceId)) then
+        xsx.Text = "Supported!"
+    else
+        xsx.Text = "Unsupported."
+    end
     xsx.TextColor3 = Color3.fromRGB(124, 124, 124)
     xsx.TextSize = 10.000
     xsx.TextTransparency = 1
@@ -785,7 +801,7 @@ function library:Introduction()
     text.Position = UDim2.new(0.912751675, 0, 0, 0)
     text.Size = UDim2.new(0, 26, 0, 21)
     text.Font = Enum.Font.Code
-    text.Text = "hash"
+    text.Text = game.PlaceId
     text.TextColor3 = Color3.fromRGB(124, 124, 124)
     text.TextSize = 10.000
     text.TextTransparency = 1
